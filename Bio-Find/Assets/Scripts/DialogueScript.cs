26a0f4628457bce47a6b8e733986eb56b0decc27
epsilon598 <hernan_dav.gutierrez@uao.edu.co>
2024-09-19 08:47:39 -0500

    SistemaDeDialogo

---
+++ Bio-Find/Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+
+    public string[] lines;
+
+    public float textSpeed=0.1f;
+
+    int index;
+
+    void Update()
+    {
+       
+    }
+
+    public void StartDialogue()
+    {
+        index = 0;
+
+        StartCoroutine(WriteLine());
+    }
+
+    IEnumerator WriteLine()
+    {
+        foreach (char letter in lines[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(WriteLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
